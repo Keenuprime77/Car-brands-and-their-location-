7715cc710307546f70a0a2fb93f52c5ffcdb6912
AARYA77-dev <keenu9826@gmail.com>
2026-07-28 11:57:10 +0530

    Refactor updateMap function to improve marker handling and add search control

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
-function updateMap(){
-console.log("this is map")
-fetch("car_location.json")
-.then(response => response.json())
-.then(rsp => {
-console.log(rsp.car_brands);
-rsp.car_brands.forEach(element => {
-latitude = element.latitude;
-longitude = element.longitude;
-var marker = L.marker([latitude,longitude]).addTo(map);
-brand = element.brand;
-headquarters = element.headquarters;
-marker.bindPopup(`<b>Brand:</b>${brand}<br><b>Headquarter:</b>${headquarters}`)
-marker.on('click', () => {
-    map.setView(marker.getLatLng(), map.getZoom());
-  });
+function updateMap() {
 
-});
-L.control.maptilerGeocoding({ apiKey: key }).addTo(map);
+  fetch("car_location.json")
+    .then(response => response.json())
+    .then(rsp => {
 
-})
-         
+      // Create one layer for all markers
+      const markersLayer = new L.LayerGroup();
+      map.addLayer(markersLayer);
+
+      rsp.car_brands.forEach(element => {
+
+        const marker = L.marker(
+          [element.latitude, element.longitude],
+          {
+            title: element.brand // Used by the search plugin
+          }
+        );
+
+        marker.bindPopup(`
+                    <b>Brand:</b> ${element.brand}<br>
+                    <b>Headquarters:</b> ${element.headquarters}
+                `);
+
+        marker.on("click", () => {
+          map.setView(marker.getLatLng(), map.getZoom());
+        });
+
+        markersLayer.addLayer(marker);
+
+      });
+
+      // Add the search control only once
+      const searchControl = new L.Control.Search({
+        layer: markersLayer,
+        propertyName: "title",
+        zoom: 6,
+        marker: false
+      });
+
+
+      searchControl.on("search:locationfound", function (e) {
+        e.layer.openPopup();
+      });
+
+      map.addControl(searchControl);
+    });
+
 }
-updateMap();
 
+updateMap();
